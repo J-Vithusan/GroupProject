bb27d372b6e1a655b919d93892de1990dc7a4f1b
PamilaNadesamoorthy <nadespamila@gmail.com>
2024-11-09 15:50:07 +0530

    sign-up aligned

--- neon_admin/src/app/(auth)/sign-in/page.tsx
+++ neon_admin/src/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div>
+    <div className='h-screen flex justify-center items-center'>
         <SignIn />
         
     </div>
